9ef2e3dc078fdf291d3c5a216ab71d1b803f6a7d
uyplayer <32848979@qq.com>
2024-08-14 13:02:33 +0800

    add symbols.py

--- text/symbols.py
+++ text/symbols.py
@@ -5,7 +5,7 @@ Defines the set of symbols used in text input to the model for Uyghur language.
 The default is a set of ASCII characters that works well for English or text that has been run
 through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details.
 '''
-from static.alphabet import character,vowels,consonants,punctuation,_vowels,_pulmonic_consonants
+from static.alphabet import character,tonic,consonants,punctuation,_vowels,_pulmonic_consonants
 
 def make_symbols(characters, phonemes, punctuations='!\'(),-.:;? ', pad='_', eos='~', bos='^'):
     _phonemes_sorted = sorted(list(phonemes))
