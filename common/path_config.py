cb29139dbb2423b17e268822e6c24abcdea078fd
uyplayer <32848979@qq.com>
2024-08-16 20:35:50 +0800

    change path_config

--- common/path_config.py
+++ common/path_config.py
@@ -1,13 +1,12 @@
 import os
 
 project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 log_dir = os.path.join(project_dir, 'logs')
 log_file = os.path.join(project_dir, 'logs/UG-TTS.log')
-
-"""-----------------------结果输出路径---------------------------"""
 data_dir = os.path.join(project_dir, 'data')
 
 if __name__ == "__main__":
-    print(data_dir)
+    print(project_dir)
     print(log_dir)
+    print(log_file)
+    print(data_dir)
