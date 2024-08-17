1e242284755cfbc73acb25494b43ac383e7313fb
uyplayer <32848979@qq.com>
2024-08-17 11:37:12 +0800

    add ipa  phoneme   toknizer

--- app/text/phoneme.py
+++ app/text/phoneme.py
@@ -4,13 +4,13 @@ from common.log_utils import get_logger
 logger = get_logger("ug_tts")
 
 
-class Phoneme (object):
+class Phoneme(object):
     _instance = None
 
     def __new__(cls):
         logger.info(f"Phoneme is initialized")
         if cls._instance is None:
-            cls._instance = super(Phoneme , cls).__new__(cls)
+            cls._instance = super(Phoneme, cls).__new__(cls)
             cls._instance._initialize()
         return cls._instance
 
@@ -18,6 +18,5 @@ class Phoneme (object):
         pass
 
 
-if __name__ == "__main__":
-    text = "ئايدا ئىككى قېتىم دەرسكە كەلمىگەن ئوقۇغۇچىلار دەرستىن چېكىندۈرۈلىدۇ."
-
+if __name__ == '__main__':
+    pass
