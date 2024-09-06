b3f4ef90daf1f7d4eea57677181bbad7e993be43
uyplayer <uyplayer@outlook.com>
2024-09-07 01:23:32 +0800

    add TextMelDataset

--- app/models/Tacotron2/attention.py
+++ app/models/Tacotron2/attention.py
@@ -2,10 +2,10 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-
 class Attention(nn.Module):
     def __init__(self, hidden_dim):
         super(Attention, self).__init__()
+        self.hidden_dim = hidden_dim
         self.Wa = nn.Linear(hidden_dim, hidden_dim)
         self.Ua = nn.Linear(hidden_dim, hidden_dim)
         self.Va = nn.Linear(hidden_dim, 1)
@@ -13,19 +13,17 @@ class Attention(nn.Module):
     def forward(self, query, keys, values):
         if query is None:
             raise ValueError("The hidden state (query) is None, which is required for attention mechanism.")
-
-        query = query.unsqueeze(1)
-        scores = self.Va(torch.tanh(self.Wa(query) + self.Ua(keys)))
-        attention_weights = F.softmax(scores, dim=1)
-        context = torch.bmm(attention_weights.transpose(1, 2), values)
+        #  query torch.Size([16, 512])  .   values ,keys   torch.Size([16, 310, 1024])
+        query = query.unsqueeze(1)  # (batch_size, 1, hidden_dim)
+        scores = self.Va(torch.tanh(self.Wa(query) + self.Ua(keys)))  # (batch_size, seq_len, 1)
+        attention_weights = F.softmax(scores, dim=1)  # (batch_size, seq_len, 1)
+        context = torch.bmm(attention_weights.transpose(1, 2), values)  # (batch_size, 1, hidden_dim)
         return context, attention_weights
 
-
 if __name__ == '__main__':
-
     batch_size = 16
-    sequence_length = 5
-    hidden_dim = 512
+    sequence_length = 199
+    hidden_dim = 1024
     attention = Attention(hidden_dim)
     query = torch.randn(batch_size, hidden_dim)
     keys = torch.randn(batch_size, sequence_length, hidden_dim)
--- app/models/Tacotron2/tacotron2.py
+++ app/models/Tacotron2/tacotron2.py
@@ -4,21 +4,20 @@ from app.models.Tacotron2.text_encoder import TextEncoder
 from app.models.Tacotron2.attention import Attention
 from app.models.Tacotron2.decoder import Decoder
 
-
 class Tacotron2(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim):
         super(Tacotron2, self).__init__()
         self.encoder = TextEncoder(vocab_size, embedding_dim, hidden_dim)
         self.attention = Attention(hidden_dim)
         self.decoder = Decoder(hidden_dim, hidden_dim, output_dim)
+        self.hidden_state_projection = nn.Linear(hidden_dim, hidden_dim)
 
     def forward(self, x, hidden_state):
-        lengths = (x != 0).sum(dim=1).cpu().long()
-        encoder_output = self.encoder(x, lengths)
+        encoder_output = self.encoder(x, hidden_state)
+        hidden_state = self.hidden_state_projection(hidden_state)
         context, attention_weights = self.attention(hidden_state, encoder_output, encoder_output)
         mel_output, hidden_state = self.decoder(context, hidden_state)
         return mel_output, attention_weights
-
 
 if __name__ == '__main__':
     pass
